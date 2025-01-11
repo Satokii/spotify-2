d1068dcd2d60b10abfcd458018f26708d0d0e533
Satoki <satokiito@gmail.com>
2025-01-11 16:33:16 +0000

    feat: create Authorisation function

---
+++ src/Authorisation.jsx
@@ -0,0 +1,18 @@
+function Authorisation() {
+    const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
+    const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI
+    const AUTH_ENDPOINT = import.meta.env.VITE_AUTH_ENDPOINT
+    const SCOPE = import.meta.env.VITE_SCOPE
+    const RESPONSE_TYPE = import.meta.env.VITE_RESPONSE_TYPE
+    const SHOW_DIALOG = import.meta.env.VITE_SHOW_DIALOG
+  
+    return (
+      <a
+        href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&scope=${SCOPE}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&show_dialog=${SHOW_DIALOG}`}
+      >
+        Log in here
+      </a>
+    );
+  }
+  
+  export default Authorisation;
