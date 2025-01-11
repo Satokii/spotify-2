5fecf42d11529cc395423fe0be3703fa30106c08
Satoki <satokiito@gmail.com>
2025-01-11 18:57:12 +0000

    feat: import DashboardMenu into Dashboard component

--- src/app/pages/dashboard/index.jsx
+++ src/app/pages/dashboard/index.jsx
@@ -1,9 +1,11 @@
+import DashboardMenu from "./components/DashboardMenu"
+
 function Dashboard() {
     return (
         <div className="dashboard-outer-container grid">
             <div className="scrollbar-dashboard">
                 <main className="dashboard grid">
-                 
+                 <DashboardMenu />
                 </main>
             </div>
         </div>
