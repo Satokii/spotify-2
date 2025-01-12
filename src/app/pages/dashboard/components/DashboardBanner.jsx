c2ae3937690ae18f8266c3a6b564aa6bd1da8f3a
Satoki <satokiito@gmail.com>
2025-01-12 16:22:58 +0000

    feat: import Queue into DashboardBanner component

--- src/app/pages/dashboard/components/DashboardBanner.jsx
+++ src/app/pages/dashboard/components/DashboardBanner.jsx
@@ -1,10 +1,11 @@
 import CurrentlyPlaying from "./dashboard-playback/CurrentlyPlaying";
+import Queue from "@/components/queue";
 
 function DashboardBanner({ currentTrack, notPlaying }) {
   return (
     <section className="dashboard--banner grid">
       <CurrentlyPlaying currentTrack={currentTrack} notPlaying={notPlaying} />
-      {/* <Queue token={token} queue={queue} setQueue={setQueue} /> */}
+      <Queue token={token} queue={queue} setQueue={setQueue} />
     </section>
   );
 }
