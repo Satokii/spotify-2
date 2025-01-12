541cbc6578e78f8e299812b87749d08ec13137e3
Satoki <satokiito@gmail.com>
2025-01-12 19:09:19 +0000

    feat: add content to VolumeControls component

--- src/components/volume/index.jsx
+++ src/components/volume/index.jsx
@@ -1,7 +1,42 @@
-function VolumeControls() {
-    return (
+import { useEffect, useState } from "react";
 
-    )
+function VolumeControls({ token }) {
+  const [availableDevices, setAvailableDevices] = useState([]);
+  const [volume, setVolume] = useState(50);
+
+  useEffect(() => {
+    // setInterval(() => {
+    getDevices(token, setAvailableDevices);
+    // }, 100);
+  }, [token]);
+
+  useEffect(() => {
+    handleDeviceVolumeChanges(availableDevices, setVolume);
+  }, [availableDevices]);
+
+  useEffect(() => {
+    changeVolume(token, volume);
+  }, [token, volume]);
+
+  return (
+    <section className="volume-controls-container grid">
+      <VolumeIcon token={token} volume={volume} setVolume={setVolume} />
+      <div className="volume-slide-container grid">
+        <input
+          className="volume-slider"
+          type="range"
+          name="volume-slider"
+          min={0}
+          max={100}
+          step={1}
+          value={volume}
+          onChange={(e) => setVolume(e.target.value)}
+          onMouseUp={(e) => setVolume(e.target.value)}
+          onMouseDown={(e) => setVolume(e.target.value)}
+        />
+      </div>
+    </section>
+  );
 }
 
-export default VolumeControls
+export default VolumeControls;
