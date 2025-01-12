e339ec249037bda9b1c21698e78fb961d20cfa05
Satoki <satokiito@gmail.com>
2025-01-12 17:06:16 +0000

    feat: import svgs into NavLibraryHeader component

--- src/components/navigation/components/NavLibraryHeader.jsx
+++ src/components/navigation/components/NavLibraryHeader.jsx
@@ -1,14 +1,19 @@
+import Image from "next/image";
+
+import LibraryIcon from "../../../assets/svgs/main-app/library-icon.svg";
+import PlusGray from "../../../assets/svgs/main-app/plus-gray.svg";
+
 function NavLibraryHeader() {
   return (
     <div className="navigation--library-header grid">
-      <img
+      <Image
         className="navigation--library-header-icon"
         src={LibraryIcon}
         alt="library icon"
       />
       <div className="navigation--library-header-text">Your Library</div>
       <button className="navigation--library-create-playlist-btn grid">
-        <img
+        <Image
           className="navigation--library-header-plus-icon"
           src={PlusGray}
           alt="plus icon"
