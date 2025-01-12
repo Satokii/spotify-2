4de5ee11e4a202238b9fcad7f86e9517a36544cf
Satoki <satokiito@gmail.com>
2025-01-12 15:05:59 +0000

    feat: add content to CurrentlyPlayingHeader component

--- src/app/pages/dashboard/components/dashboard-playback/CurrentlyPlayingHeader.jsx
+++ src/app/pages/dashboard/components/dashboard-playback/CurrentlyPlayingHeader.jsx
@@ -1,7 +1,49 @@
-function CurrentlyPlayingHeader() {
-    return (
-        <div></div>
-    )
+import { useState } from "react";
+
+function CurrentlyPlayingHeader({ currentTrack, setPulse }) {
+  const [funBtnClass, setFunBtnClass] = useState("pulse-off");
+
+  const playPulse = () => {
+    if (funBtnClass === "pulse-on") {
+      setFunBtnClass("pulse-off");
+      setPulse(false);
+    } else {
+      setFunBtnClass("pulse-on");
+      setPulse(true);
+    }
+  };
+
+  return (
+    <div className="main-playback--cur-playing-header-container grid">
+      {currentTrack.trackIsPlaying ? (
+        <h3 className="main-playback--cur-playing-header">Currently playing</h3>
+      ) : (
+        <h3 className="main-playback--cur-playing-header">Currently paused</h3>
+      )}
+      {currentTrack.trackIsPlaying ? (
+        <div className="bars grid">
+          <div className="bars__item"></div>
+          <div className="bars__item"></div>
+          <div className="bars__item"></div>
+          <div className="bars__item"></div>
+        </div>
+      ) : (
+        <div className="empty-bars grid">
+          <div className="empty-bars__item"></div>
+          <div className="empty-bars__item"></div>
+          <div className="empty-bars__item"></div>
+          <div className="empty-bars__item"></div>
+        </div>
+      )}
+      <button
+        className={`${funBtnClass} fun-btn grid`}
+        type="submit"
+        onClick={() => playPulse()}
+      >
+        Click me
+      </button>
+    </div>
+  );
 }
 
-export default CurrentlyPlayingHeader
+export default CurrentlyPlayingHeader;
