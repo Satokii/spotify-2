fc32718e088a0e73695cacb058c164020a3dc573
Satoki <satokiito@gmail.com>
2025-01-12 17:23:15 +0000

    feat: import styles into NavLibraryFilter

--- src/components/navigation/components/NavLibraryFilter.jsx
+++ src/components/navigation/components/NavLibraryFilter.jsx
@@ -1,59 +1,69 @@
-import Image from "next/image"
-import { useState } from "react"
+import Image from "next/image";
+import { useState } from "react";
 
-import CrossGray from "../../../assets/svgs/main-app/cross-gray.svg"
+import CrossGray from "../../../assets/svgs/main-app/cross-gray.svg";
+
+import "../styles/nav-library-filter.css";
 
 function NavLibraryFilter({ setShowFilter }) {
+  const INITIAL_NAV_FILTER_STATE = [
+    {
+      name: "Playlists",
+      className: "navigation--inactive-library-filter",
+    },
+    {
+      name: "Albums",
+      className: "navigation--inactive-library-filter",
+    },
+  ];
 
-    const INITIAL_NAV_FILTER_STATE = [
-        {
-            name: "Playlists",
-            className: "navigation--inactive-library-filter",
-        },
-        {
-            name: "Albums",
-            className: "navigation--inactive-library-filter",
-        }
-    ]
+  const [libraryFilter, setLibraryFilter] = useState(INITIAL_NAV_FILTER_STATE);
 
-    const [libraryFilter, setLibraryFilter] = useState(INITIAL_NAV_FILTER_STATE)
-
-    const toggleActiveLibraryFilter = (e) => {
-        if (e.target.innerText === 'Playlists') {
-            setShowFilter('Playlists')
-        }
-        if (e.target.innerText === 'Albums') {
-            setShowFilter('Albums')
-        }
-        const updatedFilter = libraryFilter.map(filter => {
-            if (filter.name === e.target.innerText) {
-                return { ...filter, className: "navigation--active-library-filter"}
-            }
-            else {
-                return { ...filter, className: "navigation--inactive-library-filter"}
-            }
-        })
-        setLibraryFilter(updatedFilter)
+  const toggleActiveLibraryFilter = (e) => {
+    if (e.target.innerText === "Playlists") {
+      setShowFilter("Playlists");
     }
-
-    const resetPlaylistFilter = () => {
-        setShowFilter('Playlists')
-        setLibraryFilter(INITIAL_NAV_FILTER_STATE)
+    if (e.target.innerText === "Albums") {
+      setShowFilter("Albums");
     }
+    const updatedFilter = libraryFilter.map((filter) => {
+      if (filter.name === e.target.innerText) {
+        return { ...filter, className: "navigation--active-library-filter" };
+      } else {
+        return { ...filter, className: "navigation--inactive-library-filter" };
+      }
+    });
+    setLibraryFilter(updatedFilter);
+  };
 
-    return (
-        <div className='navigation--library-filter-container grid'>
+  const resetPlaylistFilter = () => {
+    setShowFilter("Playlists");
+    setLibraryFilter(INITIAL_NAV_FILTER_STATE);
+  };
 
-            {libraryFilter.map((filter, index) => (
-                <div className={filter.className} key={`${filter.name}-${index}`} onClick={e => toggleActiveLibraryFilter(e)}>
-                    {filter.name}
-                </div>
-            ))}
-            <div className="navigation--library-filter-none-container grid" onClick={() => resetPlaylistFilter()}>
-                <Image className="navigation--library-filter-none-icon" src={CrossGray} alt="cross icon" />
-            </div>
+  return (
+    <div className="navigation--library-filter-container grid">
+      {libraryFilter.map((filter, index) => (
+        <div
+          className={filter.className}
+          key={`${filter.name}-${index}`}
+          onClick={(e) => toggleActiveLibraryFilter(e)}
+        >
+          {filter.name}
         </div>
-    )
+      ))}
+      <div
+        className="navigation--library-filter-none-container grid"
+        onClick={() => resetPlaylistFilter()}
+      >
+        <Image
+          className="navigation--library-filter-none-icon"
+          src={CrossGray}
+          alt="cross icon"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default NavLibraryFilter
+export default NavLibraryFilter;
