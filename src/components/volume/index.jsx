216af58ae736145388e221f272558f44948109d4
Satoki <satokiito@gmail.com>
2025-01-12 19:14:24 +0000

    feat: create handleDeviceVolumeChanges function

--- src/components/volume/index.jsx
+++ src/components/volume/index.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import getDevices from "./components/getDevices";
+import handleDeviceVolumeChanges from "./components/handleDeviceVolumeChanges";
 
 import "./styles/volume-controls-container.css"
 import "./styles/volume-slider.css"
