98d5a1ad8d26517373b4f70ef32adc2f77c0c1d0
Satoki <satokiito@gmail.com>
2025-01-12 17:24:18 +0000

    feat: import NavLibraryFilter into Navigation component

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import NavMainLinks from "./components/NavMainLinks";
 import NavLibraryHeader from "./components/NavLibraryHeader";
-// import NavLibraryFilter from "./components/NavLibraryFilter";
+import NavLibraryFilter from "./components/NavLibraryFilter";
 // import NavLibraryPlaylists from "./components/NavLibraryPlaylists";
 
 import "./styles/navigation.css";
@@ -14,8 +14,8 @@ function Navigation({ token }) {
       <NavMainLinks />
       <div className="navigation--library-container grid">
         <NavLibraryHeader />
-        {/* <NavLibraryFilter setShowFilter={setShowFilter} />
-        <NavLibraryPlaylists token={token} showFilter={showFilter} /> */}
+        <NavLibraryFilter setShowFilter={setShowFilter} />
+        {/* <NavLibraryPlaylists token={token} showFilter={showFilter} /> */}
       </div>
     </section>
   );
