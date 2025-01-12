ee173cf19355de2cfffe41e5feee746b803001e0
Satoki <satokiito@gmail.com>
2025-01-12 15:22:48 +0000

    feat: add content to CurrentlyPlayingTrack component

--- src/app/pages/dashboard/components/dashboard-playback/CurrentlyPlayingTrack.jsx
+++ src/app/pages/dashboard/components/dashboard-playback/CurrentlyPlayingTrack.jsx
@@ -1,7 +1,57 @@
-function CurrentlyPlayingTrack() {
-    return (
-        <div></div>
-    )
+import { Link } from "react-router-dom";
+
+function CurrentlyPlayingTrack({ currentTrack, notPlaying, pulse }) {
+  const togglePulse = () => {
+    if (pulse) {
+      return "main-playback--pulse-img";
+    } else return "";
+  };
+
+  return (
+    <>
+      {notPlaying ? (
+        <div className="main-playback--no-track-playing grid">
+          Play a song on Spotify
+        </div>
+      ) : (
+        <div
+          className="main-playback--cur-playing-item grid"
+          id={currentTrack.trackId}
+        >
+          {currentTrack.trackImageLength ? (
+            <Link
+              to={`/album/${currentTrack.albumId}/${currentTrack.artistId}`}
+              onClick={scrollToTop}
+            >
+              <img
+                className={togglePulse()}
+                src={currentTrack.trackImage}
+                alt={`${currentTrack.trackName}-image`}
+              />
+            </Link>
+          ) : (
+            <div></div>
+          )}
+          <div className="main-playback--cur-playing-details grid">
+            <Link
+              className="main-playback--cur-playing-title"
+              to={`/album/${currentTrack.albumId}/${currentTrack.artistId}`}
+              onClick={scrollToTop}
+            >
+              {currentTrack.trackName}
+            </Link>
+            <Link
+              className="main-playback--cur-playing-artist"
+              to={`/artist/${currentTrack.artistId}`}
+              onClick={scrollToTop}
+            >
+              {currentTrack.trackArtist}
+            </Link>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
-export default CurrentlyPlayingTrack
+export default CurrentlyPlayingTrack;
