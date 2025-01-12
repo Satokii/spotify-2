2d68350c642f6543af037d9a8c0867b18d12b2a6
Satoki <satokiito@gmail.com>
2025-01-12 14:59:59 +0000

    feat: add content to CurrentlyPlaying component

--- src/app/pages/dashboard/components/dashboard-playback/CurrentlyPlaying.jsx
+++ src/app/pages/dashboard/components/dashboard-playback/CurrentlyPlaying.jsx
@@ -1,7 +1,17 @@
-function CurrentlyPlaying() {
-    return (
-        <div>Currently playing</div>
-    )
+function CurrentlyPlaying({ currentTrack, notPlaying }) {
+    
+  const [pulse, setPulse] = useState(false);
+
+  return (
+    <div className="main-playback--cur-playing-container grid">
+      <CurrentlyPlayingHeader currentTrack={currentTrack} setPulse={setPulse} />
+      <CurrentlyPlayingTrack
+        currentTrack={currentTrack}
+        notPlaying={notPlaying}
+        pulse={pulse}
+      />
+    </div>
+  );
 }
 
-export default CurrentlyPlaying
+export default CurrentlyPlaying;
