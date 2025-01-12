320c454aab4af7250273ded3fc2a54e92c006fb0
Satoki <satokiito@gmail.com>
2025-01-12 17:03:05 +0000

    feat: add content to NavLibraryHeader component

--- src/components/navigation/components/NavLibraryHeader.jsx
+++ src/components/navigation/components/NavLibraryHeader.jsx
@@ -1,7 +1,21 @@
 function NavLibraryHeader() {
-    return (
-        <div></div>
-    )
+  return (
+    <div className="navigation--library-header grid">
+      <img
+        className="navigation--library-header-icon"
+        src={LibraryIcon}
+        alt="library icon"
+      />
+      <div className="navigation--library-header-text">Your Library</div>
+      <button className="navigation--library-create-playlist-btn grid">
+        <img
+          className="navigation--library-header-plus-icon"
+          src={PlusGray}
+          alt="plus icon"
+        />
+      </button>
+    </div>
+  );
 }
 
-export default NavLibraryHeader
+export default NavLibraryHeader;
