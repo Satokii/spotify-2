db175654cdc638e73fc2b840da6f0fc17404e9b7
Satoki <satokiito@gmail.com>
2025-01-12 16:29:33 +0000

    feat: add content to Navigation component

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -1,7 +1,24 @@
-function Navigation() {
-    return (
-        <div></div>
-    )
+import { useState } from "react";
+import NavMainLinks from "./components/NavMainLinks";
+import NavLibraryHeader from "./components/NavLibraryHeader";
+import NavLibraryFilter from "./components/NavLibraryFilter";
+import NavLibraryPlaylists from "./components/NavLibraryPlaylists";
+
+import "./styles/navigation.css";
+
+function Navigation({ token }) {
+  const [showFilter, setShowFilter] = useState("Playlists");
+
+  return (
+    <section className="navigation--container grid">
+      <NavMainLinks />
+      <div className="navigation--library-container grid">
+        <NavLibraryHeader />
+        <NavLibraryFilter setShowFilter={setShowFilter} />
+        <NavLibraryPlaylists token={token} showFilter={showFilter} />
+      </div>
+    </section>
+  );
 }
 
-export default Navigation
+export default Navigation;
