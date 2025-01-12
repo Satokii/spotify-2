60d02ae05233955489c59b8e942c23a5f2518ecc
Satoki <satokiito@gmail.com>
2025-01-12 18:44:10 +0000

    feat: add MusicPlayer content

--- src/components/music-player/index.jsx
+++ src/components/music-player/index.jsx
@@ -1,7 +1,45 @@
-function MusicPlayer() {
-    return (
+import { useEffect, useState } from "react";
+import getQueue from "../queue/functions/getQueue";
 
-    )
+function MusicPlayer({ token, currentTrack, setCurrentTrack, setQueue }) {
+  // HANDLE SLIDER POSITION - BOTH MANUAL AND AUTOMATIC
+  const [sliderVal, setSliderVal] = useState(0);
+  const [manualSeekVal, setManualSeekVal] = useState();
+
+  const timeElapsed = Number(
+    ((currentTrack.trackProgress / currentTrack.trackDuration) * 100).toFixed(0)
+  );
+
+  useEffect(() => {
+    setSliderVal(timeElapsed);
+  }, [timeElapsed]);
+
+  useEffect(() => {
+    seekToPosition(token, manualSeekVal);
+  }, [manualSeekVal, token]);
+
+  // CHECK TO UPDATE QUEUE WHEN TRACK CHANGES
+  useEffect(() => {
+    if (sliderVal >= 1 && sliderVal <= 3) {
+      getQueue(token, setQueue);
+    }
+  }, [setQueue, sliderVal, token]);
+
+  return (
+    <section className="song-player-container grid">
+      <SongControls
+        token={token}
+        currentTrack={currentTrack}
+        setCurrentTrack={setCurrentTrack}
+      />
+      <TrackSlider
+        currentTrack={currentTrack}
+        sliderVal={sliderVal}
+        setSliderVal={setSliderVal}
+        setManualSeekVal={setManualSeekVal}
+      />
+    </section>
+  );
 }
 
-export default MusicPlayer
+export default MusicPlayer;
