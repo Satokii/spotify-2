9cfb486c0d2de80b1ccf66e8385722aab5bafba8
Satoki <satokiito@gmail.com>
2025-01-12 15:46:42 +0000

    feat: add show page/log in condition depending on presence of valid token

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,8 +15,8 @@ import "./keyframes/fade-in.css";
 export default function Home() {
   const [token, setToken] = useState("");
 
-   //  QUEUE
-   const [queue, setQueue] = useState([])
+  //  QUEUE
+  const [queue, setQueue] = useState([]);
 
   useEffect(() => {
     const hash = window.location.hash;
@@ -33,51 +33,67 @@ export default function Home() {
     setToken(token);
   }, []);
 
-    // FETCH CURRENTLY PLAYING TRACK
-    const [currentTrack, setCurrentTrack] = useState(CURRENT_TRACK_INITIAL_STATE);
-    const [currentTrackArtists, setCurrentTrackArtists] = useState([])
-    const [notPlaying, setNotPlaying] = useState(null);
-  
-    useEffect(() => {
-      const getCurrentTrack = async () => {
-        const { data } = await axios.get(
-          "https://api.spotify.com/v1/me/player/currently-playing",
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-        if (!data) setNotPlaying(true);
-        else {
-          const { item } = data;
-          // console.log(data)
-          setCurrentTrackArtists(item.artists)
-          setCurrentTrack({
-            trackId: item.id,
-            albumId: item.album.id,
-            artistId: item.artists[0].id,
-            trackImageLength: item.album.images.length,
-            trackImage: item.album.images[0].url,
-            trackName: item.name,
-            trackArtist: item.artists[0].name,
-            trackIsPlaying: data.is_playing,
-            trackProgress: data.progress_ms,
-            trackDuration: item.duration_ms,
-          });
+  // FETCH CURRENTLY PLAYING TRACK
+  const [currentTrack, setCurrentTrack] = useState(CURRENT_TRACK_INITIAL_STATE);
+  const [currentTrackArtists, setCurrentTrackArtists] = useState([]);
+  const [notPlaying, setNotPlaying] = useState(null);
+
+  useEffect(() => {
+    const getCurrentTrack = async () => {
+      const { data } = await axios.get(
+        "https://api.spotify.com/v1/me/player/currently-playing",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         }
-      };
-      // setInterval(() => {
-        getCurrentTrack();
-      // }, 1000);
-    }, [ token]);
+      );
+      if (!data) setNotPlaying(true);
+      else {
+        const { item } = data;
+        // console.log(data)
+        setCurrentTrackArtists(item.artists);
+        setCurrentTrack({
+          trackId: item.id,
+          albumId: item.album.id,
+          artistId: item.artists[0].id,
+          trackImageLength: item.album.images.length,
+          trackImage: item.album.images[0].url,
+          trackName: item.name,
+          trackArtist: item.artists[0].name,
+          trackIsPlaying: data.is_playing,
+          trackProgress: data.progress_ms,
+          trackDuration: item.duration_ms,
+        });
+      }
+    };
+    // setInterval(() => {
+    getCurrentTrack();
+    // }, 1000);
+  }, [token]);
 
   return (
     <BrowserRouter>
       <div className="container grid">
         <Routes>
           <Route path="/spotify-login" element={<WelcomePage />}></Route>
-          <Route path="/" element={<Dashboard token={token} queue={queue} setQueue={setQueue} setToken={setToken} currentTrack={currentTrack} notPlaying={notPlaying} />}></Route>
+          <Route
+            path="/"
+            element={
+              token ? (
+                <Dashboard
+                  token={token}
+                  queue={queue}
+                  setQueue={setQueue}
+                  setToken={setToken}
+                  currentTrack={currentTrack}
+                  notPlaying={notPlaying}
+                />
+              ) : (
+                <WelcomePage />
+              )
+            }
+          ></Route>
         </Routes>
       </div>
     </BrowserRouter>
