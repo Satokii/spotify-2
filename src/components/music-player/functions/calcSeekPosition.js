eff3b1c1ed4a034b3d4ca4e6fb18df9a36528e8b
Satoki <satokiito@gmail.com>
2025-01-12 18:50:25 +0000

    feat: create calcSeekPosition function

---
+++ src/components/music-player/functions/calcSeekPosition.js
@@ -0,0 +1,7 @@
+const calcSeekPosition = async (slidebarPosition, currentTrack, setManualSeekVal) => {
+    const currentTimeinMs = (slidebarPosition / 100) * currentTrack.trackDuration;
+    setManualSeekVal(Number(currentTimeinMs.toFixed(0)));
+  };
+  
+  export default calcSeekPosition;
+  
