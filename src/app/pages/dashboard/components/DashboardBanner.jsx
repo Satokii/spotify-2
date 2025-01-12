61352f62f89886295ac9a2372a69ce569a6773b1
Satoki <satokiito@gmail.com>
2025-01-12 14:54:17 +0000

    feat: add DashboardBanner content

--- src/app/pages/dashboard/components/DashboardBanner.jsx
+++ src/app/pages/dashboard/components/DashboardBanner.jsx
@@ -1,7 +1,10 @@
 function DashboardBanner() {
-    return (
-        <div>Banner</div>
-    )
+  return (
+    <section className="dashboard--banner grid">
+      <CurrentlyPlaying currentTrack={currentTrack} notPlaying={notPlaying} />
+      <Queue token={token} queue={queue} setQueue={setQueue} />
+    </section>
+  );
 }
 
-export default DashboardBanner
+export default DashboardBanner;
