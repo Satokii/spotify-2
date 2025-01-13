adcd10f87385866210ccf4257e634e51a96db0e3
Satoki <satokiito@gmail.com>
2025-01-13 14:29:42 +0000

    feat: import SearchBar component into SearchPageNavContent

--- src/app/pages/search/components/SearchPageNavContent.jsx
+++ src/app/pages/search/components/SearchPageNavContent.jsx
@@ -7,6 +7,7 @@ import ForwardArrow from "../../../../assets/svgs/main-app/forward-arrow.svg"
 import NotificationBell from "../../../../assets/svgs/main-app/noti-bell.svg"
 import ProfileIcon from "../../../../assets/svgs/main-app/profile-icon.svg"
 import LogOutBtn from "../../../../assets/svgs/main-app/log-out.svg"
+import SearchBar from "./SearchBar";
 
 import "../styles/search-page-nav-content.css"
 
