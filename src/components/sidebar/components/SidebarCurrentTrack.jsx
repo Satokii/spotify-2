0713cf9e2f8a175c7b0e32a4e5453b1ec07d4883
Satoki <satokiito@gmail.com>
2025-01-13 13:16:36 +0000

    feat: add content to SidebarTrackArtists

--- src/components/sidebar/components/SidebarCurrentTrack.jsx
+++ src/components/sidebar/components/SidebarCurrentTrack.jsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import { Link } from "react-router-dom";
 import scrollToTop from "@/shared-functions/scrollToTop";
 import fixLengthPreviews from "@/shared-functions/fixLengthPreviews";
 
