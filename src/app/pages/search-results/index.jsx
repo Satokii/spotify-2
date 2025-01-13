49aca0e19724f4a52c0baec560f7b37a8d3b0784
Satoki <satokiito@gmail.com>
2025-01-13 15:30:02 +0000

    feat: temporarily disable SearchResultsPlaylists due to bug with API

--- src/app/pages/search-results/index.jsx
+++ src/app/pages/search-results/index.jsx
@@ -1,6 +1,7 @@
 import SearchResultsTracks from './components/SearchResultsTracks';
 import SearchResultsArtists from './components/SearchResultsArtists';
 import SearchResultsAlbums from './components/SearchResultsAlbums';
+// import SearchResultsPlaylists from './components/SearchResultsPlaylists';
 
 import './styles/search-results-generic.css'
 
