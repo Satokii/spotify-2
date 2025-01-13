35de4565f24579ec4f9c169e99c0b8170abb0e6d
Satoki <satokiito@gmail.com>
2025-01-13 15:51:39 +0000

    feat: create css file for MainNav styles

--- src/components/main-nav/index.jsx
+++ src/components/main-nav/index.jsx
@@ -7,6 +7,8 @@ import NotificationBell from "../../assets/svgs/main-app/noti-bell.svg";
 import ProfileIcon from "../../assets/svgs/main-app/profile-icon.svg";
 import LogOutBtn from "../../assets/svgs/main-app/log-out.svg";
 
+import "./styles/main-nav.css";
+
 function MainNav({ setToken }) {
   const navigate = useNavigate();
 
