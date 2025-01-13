26735d684174f9d4a739833c08e00874753049cd
Satoki <satokiito@gmail.com>
2025-01-13 14:56:43 +0000

    feat: create GetTrackArtists function

---
+++ src/shared-functions/GetTrackArtists.jsx
@@ -0,0 +1,54 @@
+import { Link } from "react-router-dom";
+import scrollToTop from "./scrollToTop";
+
+const GetTrackArtists = (track) => {
+  const numArtists = track.artists.length;
+  if (numArtists === 1)
+    return (
+      <Link
+        id={track.artists[0].id}
+        className="album-page--artist-name"
+        to={`/artist/${track.artists[0].id}`}
+        onClick={scrollToTop}
+      >
+        {track.artists[0].name}
+      </Link>
+    );
+  else {
+    const separated = track.artists.map((artist, index) => {
+      if (index === numArtists - 1)
+        return (
+          <Link
+            id={artist.id}
+            className="album-page--artist-name"
+            to={`/artist/${artist.id}`}
+            onClick={scrollToTop}
+          >
+            {artist.name}
+          </Link>
+        );
+      else if (index === 0)
+        return (
+          <Link
+            id={artist.id}
+            className="album-page--artist-name"
+            to={`/artist/${artist.id}`}
+            onClick={scrollToTop}
+          >{`${artist.name}, `}</Link>
+        );
+      else
+        return (
+          <Link
+            id={artist.id}
+            className="album-page--artist-name"
+            to={`/artist/${artist.id}`}
+            onClick={scrollToTop}
+          >{`${artist.name}... `}</Link>
+        );
+    });
+    if (separated.length > 2) return separated.slice(0, 2);
+    else return separated;
+  }
+};
+
+export default GetTrackArtists;
