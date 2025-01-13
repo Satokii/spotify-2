0c057c1f2092f7e32755b431a29be9737f111265
Satoki <satokiito@gmail.com>
2025-01-13 15:57:35 +0000

    feat: import sleep function into Album page

--- src/app/pages/album/index.jsx
+++ src/app/pages/album/index.jsx
@@ -4,6 +4,7 @@ import { usePalette } from "react-palette";
 import AlbumTopNav from "./components/AlbumTopNav";
 import getAlbum from "./functions/getAlbum";
 import getArtist from "../artist/functions/getArtist";
+import sleep from "@/shared-functions/sleep";
 
 function Album({ token, setToken }) {
   const [albumInfo, setAlbumInfo] = useState({});
