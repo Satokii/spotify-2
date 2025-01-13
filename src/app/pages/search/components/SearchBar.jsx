235f520c4b3a66c162d384b44604f481015c7dc2
Satoki <satokiito@gmail.com>
2025-01-13 14:27:00 +0000

    feat: create SearchBar component

---
+++ src/app/pages/search/components/SearchBar.jsx
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { useState } from "react";
+
+function SearchBar({
+  token,
+  setTrackResults,
+  setArtistResults,
+  setAlbumResults,
+  setPlaylistResults,
+}) {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const getSearchResult = async (e) => {
+    e.preventDefault();
+    const { data } = await axios.get("https://api.spotify.com/v1/search", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      params: {
+        q: searchQuery,
+        type: "track,artist,album,playlist",
+        limit: 8,
+      },
+    });
+    setTrackResults(data.tracks.items);
+    setArtistResults(data.artists.items);
+    setAlbumResults(data.albums.items);
+    setPlaylistResults(data.playlists.items);
+  };
+
+  return (
+    <section className="searchbar grid">
+      <form
+        className="searchbar--form grid"
+        onSubmit={(e) => getSearchResult(e)}
+      >
+        <input
+          type="text"
+          placeholder="What do you want to listen to?"
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button className="search" type="reset"></button>
+      </form>
+    </section>
+  );
+}
+
+export default SearchBar;
