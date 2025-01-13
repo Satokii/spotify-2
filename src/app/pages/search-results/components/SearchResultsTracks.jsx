77998a80170875c3adfe75fbec13b91ffe80285a
Satoki <satokiito@gmail.com>
2025-01-13 14:49:45 +0000

    feat: create SearchResultsTracks component

---
+++ src/app/pages/search-results/components/SearchResultsTracks.jsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+
+function SearchResultsTracks({ trackResults }) {
+  return (
+    <div className="search-results-tracks--container grid">
+      <h3 className="search-results-tracks--header">Songs</h3>
+      <ul className="search-results-tracks--list grid">
+        {trackResults.map((track, index) => (
+          <li
+            className="search-results-tracks--track grid"
+            key={`${track.id}-${index}`}
+          >
+            {track.album.images.length ? (
+              <img
+                src={track.album.images[0].url}
+                alt={`${track.name} image`}
+              />
+            ) : (
+              <div>No Image</div>
+            )}
+            <div className="search-results-tracks--track-text-container grid">
+              <Link
+                className="search-results-tracks--track-name"
+                to={`/album/${track.album.id}/${track.artists[0].id}`}
+                onClick={scrollToTop}
+              >
+                {fixLengthSearchTracks(track.name)}
+              </Link>
+              <p className="search-results-tracks--track-artist">
+                {GetSearchTrackArtists(track)}
+              </p>
+            </div>
+            <div className="search-results-tracks--track-time">
+              {calcTrackTime(track.duration_ms)}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default SearchResultsTracks;
