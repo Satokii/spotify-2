68e782adbec50c4870dd5d5e6875b9b0b8d71fc9
Satoki <satokiito@gmail.com>
2025-01-13 13:11:37 +0000

    feat: add content to SidebarCurrentTrack component

--- src/components/sidebar/components/SidebarCurrentTrack.jsx
+++ src/components/sidebar/components/SidebarCurrentTrack.jsx
@@ -1,7 +1,36 @@
-function SidebarCurrentTrack() {
-    return (
+import Link from "next/link";
+import scrollToTop from "@/shared-functions/scrollToTop";
 
-    )
+function SidebarCurrentTrack({ currentTrack, currentTrackArtists }) {
+  return (
+    <div className="sidebar--current-track-container grid">
+      <Link
+        className="sidebar--current-track-img-container"
+        to={`/album/${currentTrack.albumId}/${currentTrack.artistId}`}
+        onClick={scrollToTop}
+      >
+        <div>
+          {currentTrack.trackImage ? (
+            <img
+              className="sidebar--current-track-img"
+              src={currentTrack.trackImage}
+              alt="current track img"
+            />
+          ) : (
+            <div>Play a song on Spotify</div>
+          )}
+        </div>
+      </Link>
+      <Link
+        className="sidebar--current-track-name"
+        to={`/album/${currentTrack.albumId}/${currentTrack.artistId}`}
+        onClick={scrollToTop}
+      >
+        {fixLengthPreviews(currentTrack.trackName)}
+      </Link>
+      <div>{CurrentTrackArtists(currentTrackArtists)}</div>
+    </div>
+  );
 }
 
-export default SidebarCurrentTrack
+export default SidebarCurrentTrack;
