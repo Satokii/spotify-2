adc339f67b90a503b276bdcb18c53e482dfcfd9a
Satoki <satokiito@gmail.com>
2025-01-13 15:03:41 +0000

    feat: create SearchResultsArtists component

---
+++ src/app/pages/search-results/components/SearchResultsArtists.jsx
@@ -0,0 +1,45 @@
+import { Link } from "react-router-dom";
+import scrollToTop from "@/shared-functions/scrollToTop";
+
+function SearchResultsArtists({ artistResults }) {
+  return (
+    <div className="search-results-card--container grid">
+      <h3 className="search-results-card-header">Artists</h3>
+      <ul className="search-results-card--list grid">
+        {artistResults.map((artist, index) => (
+          <Link
+            className="search-results-card--single grid"
+            key={`${artist.id}-${index}`}
+            to={`/artist/${artist.id}`}
+            onClick={scrollToTop}
+          >
+            <div className="search-results-card--img-outer-container">
+              {artist.images.length ? (
+                <div className="search-results-card--img-container">
+                  <img
+                    className="search-results--card-img"
+                    src={artist.images[0].url}
+                    alt="artist image"
+                  />
+                  <img
+                    className="search-results-card--play"
+                    src={PlayGreen}
+                    alt="play button"
+                  />
+                </div>
+              ) : (
+                <div className="search-results--card-img"></div>
+              )}
+            </div>
+            <p className="search-results--card-name">
+              {fixLengthSearch(artist.name)}
+            </p>
+            <p className="search-results--card-type">Artist</p>
+          </Link>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default SearchResultsArtists;
