f77f19c822cea34263047d4221dafe0f333f6f3f
Satoki <satokiito@gmail.com>
2025-01-13 14:24:28 +0000

    feat: create css file for SearchPageNavContent styles

--- src/app/pages/search/components/SearchPageNavContent.jsx
+++ src/app/pages/search/components/SearchPageNavContent.jsx
@@ -7,6 +7,8 @@ import NotificationBell from "../../../../assets/svgs/main-app/noti-bell.svg"
 import ProfileIcon from "../../../../assets/svgs/main-app/profile-icon.svg"
 import LogOutBtn from "../../../../assets/svgs/main-app/log-out.svg"
 
+import "../styles/search-page-nav-content.css"
+
 function SearchPageNavContent({
   token,
   setToken,
