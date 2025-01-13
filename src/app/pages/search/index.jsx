078b94e6e63e468421d3db03aa08c7489a01d3a6
Satoki <satokiito@gmail.com>
2025-01-13 14:12:29 +0000

    feat: add content to Search component

--- src/app/pages/search/index.jsx
+++ src/app/pages/search/index.jsx
@@ -1,7 +1,37 @@
-function Search() {
-    return (
+import { useState } from "react";
 
-    )
+function Search({ token, setToken }) {
+  const [trackResults, setTrackResults] = useState([]);
+  const [artistResults, setArtistResults] = useState([]);
+  const [albumResults, setAlbumResults] = useState([]);
+  const [playlistResults, setPlaylistResults] = useState([]);
+
+  return (
+    <div className="search-page--outer-container grid">
+      <div className="scrollbar-search-page">
+        <section className="search-page-container grid">
+          <SearchPageTopNav
+            token={token}
+            setToken={setToken}
+            setTrackResults={setTrackResults}
+            setArtistResults={setArtistResults}
+            setAlbumResults={setAlbumResults}
+            setPlaylistResults={setPlaylistResults}
+          />
+          {trackResults.length > 0 ? (
+            <SearchResultsPage
+              trackResults={trackResults}
+              artistResults={artistResults}
+              albumResults={albumResults}
+              playlistResults={playlistResults}
+            />
+          ) : (
+            <SearchPageBrowse />
+          )}
+        </section>
+      </div>
+    </div>
+  );
 }
 
-export default Search
+export default Search;
