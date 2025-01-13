42f4e2633962dd086dd40738f726b278a0d774b8
Satoki <satokiito@gmail.com>
2025-01-13 14:15:31 +0000

    feat: create SearchPageTopNav component

---
+++ src/app/pages/search/components/SearchPageTopNav.jsx
@@ -0,0 +1,25 @@
+function SearchPageTopNav({
+  token,
+  setToken,
+  setTrackResults,
+  setArtistResults,
+  setAlbumResults,
+  setPlaylistResults,
+}) {
+  return (
+    <div className="search-page--menu-backing">
+      <section className="search-page--menu-container grid">
+        <SearchPageNavContent
+          token={token}
+          setToken={setToken}
+          setTrackResults={setTrackResults}
+          setArtistResults={setArtistResults}
+          setAlbumResults={setAlbumResults}
+          setPlaylistResults={setPlaylistResults}
+        />
+      </section>
+    </div>
+  );
+}
+
+export default SearchPageTopNav;
