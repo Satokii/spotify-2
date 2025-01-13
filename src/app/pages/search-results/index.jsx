2edf523700ada37e79479dcc303b88be9eb83c2a
Satoki <satokiito@gmail.com>
2025-01-13 14:45:28 +0000

    feat: create SearchResults page

---
+++ src/app/pages/search-results/index.jsx
@@ -0,0 +1,17 @@
+function SearchResults({
+  trackResults,
+  artistResults,
+  albumResults,
+  playlistResults,
+}) {
+  return (
+    <section className="search-results--container grid">
+      <SearchResultsTracks trackResults={trackResults} />
+      <SearchResultsArtists artistResults={artistResults} />
+      <SearchResultsAlbums albumResults={albumResults} />
+      <SearchResultsPlaylists playlistResults={playlistResults} />
+    </section>
+  );
+}
+
+export default SearchResults;
