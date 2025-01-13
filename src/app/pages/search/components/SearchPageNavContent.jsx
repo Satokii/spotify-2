acb997ca1a820fde526578953f1d1f2541d22416
Satoki <satokiito@gmail.com>
2025-01-13 14:21:40 +0000

    feat: import svgs into SearchPageNavContent component

--- src/app/pages/search/components/SearchPageNavContent.jsx
+++ src/app/pages/search/components/SearchPageNavContent.jsx
@@ -1,6 +1,12 @@
 import { useNavigate } from "react-router-dom";
 import Logout from "@/Logout";
 
+import BackArrow from "../../../../assets/svgs/main-app/back-arrow.svg"
+import ForwardArrow from "../../../../assets/svgs/main-app/forward-arrow.svg"
+import NotificationBell from "../../../../assets/svgs/main-app/noti-bell.svg"
+import ProfileIcon from "../../../../assets/svgs/main-app/profile-icon.svg"
+import LogOutBtn from "../../../../assets/svgs/main-app/log-out.svg"
+
 function SearchPageNavContent({
   token,
   setToken,
