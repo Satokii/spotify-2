23f35fceb8c52de2302e24b630e765fe018ed053
Satoki <satokiito@gmail.com>
2025-01-13 12:23:47 +0000

    feat: create css file for dashboard banner styles

--- src/app/pages/dashboard/components/DashboardBanner.jsx
+++ src/app/pages/dashboard/components/DashboardBanner.jsx
@@ -1,6 +1,8 @@
 import CurrentlyPlaying from "./dashboard-playback/CurrentlyPlaying";
 import Queue from "@/components/queue";
 
+import "../styles/dashboard-banner.css"
+
 function DashboardBanner({ token, queue, setQueue, currentTrack, notPlaying  }) {
   return (
     <section className="dashboard--banner grid">
