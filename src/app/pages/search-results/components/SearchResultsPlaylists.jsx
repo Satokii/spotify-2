155224db453c19d85d813f20d8c8b9cbc15cd185
Satoki <satokiito@gmail.com>
2025-01-13 15:16:57 +0000

    feat: create SearchResultsPlaylists component

---
+++ src/app/pages/search-results/components/SearchResultsPlaylists.jsx
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom";
+import Image from "next/image";
+import fixLengthSearch from "@/shared-functions/fixLengthSearch";
+import PlayGreen from "../../../../assets/svgs/main-app/main-play-btn.svg";
+import scrollToTop from "@/shared-functions/scrollToTop";
+
+function SearchResultsPlaylists({ playlistResults }) {
+  return (
+    <div className="search-results-card--container grid">
+      <h3 className="search-results-card-header">Playlists</h3>
+      <ul className="search-results-card--list grid">
+        {playlistResults.map((playlist, index) => (
+          <Link
+            className="search-results-card--single grid"
+            key={`${playlist.id}-${index}`}
+            to={`/user-playlist/${playlist.id}`}
+            onClick={scrollToTop}
+          >
+            <div className="search-results-card--img-outer-container">
+              {playlist.images.length ? (
+                <div className="search-results-card--img-container">
+                  <img
+                    className="search-results--card-img-sq"
+                    src={playlist.images[0].url}
+                    alt="playlist image"
+                  />
+                  <Image
+                    className="search-results-card--play"
+                    src={PlayGreen}
+                    alt="play button"
+                  />
+                </div>
+              ) : (
+                <div className="search-results--card-img-sq"></div>
+              )}
+            </div>
+            <p className="search-results--card-name">
+              {fixLengthSearch(playlist.name)}
+            </p>
+            <p className="search-results--card-type">
+              {fixLengthSearch(playlist.owner.display_name)}
+            </p>
+          </Link>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default SearchResultsPlaylists;
