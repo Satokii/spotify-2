f7bc80f56966e211b94b79f77a41315bf7fdb0fd
Satoki <satokiito@gmail.com>
2025-01-13 14:25:45 +0000

    refactor: update img tags to Next.js Image in SearchPageNavContent component

--- src/app/pages/search/components/SearchPageNavContent.jsx
+++ src/app/pages/search/components/SearchPageNavContent.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import Logout from "@/Logout";
+import Image from "next/image";
 
 import BackArrow from "../../../../assets/svgs/main-app/back-arrow.svg"
 import ForwardArrow from "../../../../assets/svgs/main-app/forward-arrow.svg"
@@ -23,10 +24,10 @@ function SearchPageNavContent({
     <>
       <div className="search-page-nav--container grid">
         <div onClick={() => navigate(-1)}>
-          <img src={BackArrow} alt="back arrow" />
+          <Image src={BackArrow} alt="back arrow" />
         </div>
         <div onClick={() => navigate(1)}>
-          <img src={ForwardArrow} alt="forward arrow" />
+          <Image src={ForwardArrow} alt="forward arrow" />
         </div>
       </div>
       <SearchBar
@@ -38,21 +39,21 @@ function SearchPageNavContent({
       />
       <div className="search-page-nav--icons-container grid">
         <div>
-          <img
+          <Image
             className="search-page-nav--notification-bell"
             src={NotificationBell}
             alt="notification bell"
           />
         </div>
         <div>
-          <img
+          <Image
             className="search-page-nav--profile"
             src={ProfileIcon}
             alt="profile icon"
           />
         </div>
         <div onClick={() => Logout(setToken)}>
-          <img
+          <Image
             className="search-page-nav--log-out"
             src={LogOutBtn}
             alt="log out button"
