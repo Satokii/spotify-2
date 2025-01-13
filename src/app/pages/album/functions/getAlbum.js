75b83857e9e379e3ea4f66e1f2c01c2ca49441e8
Satoki <satokiito@gmail.com>
2025-01-13 15:54:20 +0000

    feat: create getAlbum function

---
+++ src/app/pages/album/functions/getAlbum.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const getAlbum = async (token, albumId, setAlbumTracksArr, setAlbumInfo, setCopyrights) => {
+    const { data } = await axios.get(
+      `https://api.spotify.com/v1/albums/${albumId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    setAlbumTracksArr(data.tracks.items);
+    setCopyrights(data.copyrights)
+    setAlbumInfo({
+      name: data.name,
+      img: data.images[0].url,
+      type: data.album_type,
+      releaseDate: data.release_date,
+      totalTracks: data.total_tracks,
+    });
+};
+
+export default getAlbum
--- src/app/pages/album/index.jsx
+++ src/app/pages/album/index.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { usePalette } from "react-palette";
 import AlbumTopNav from "./components/AlbumTopNav";
+import getAlbum from "./functions/getAlbum";
 
 function Album({ token, setToken }) {
   const [albumInfo, setAlbumInfo] = useState({});
