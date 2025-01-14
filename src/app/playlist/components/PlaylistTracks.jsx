0a1fd8b116bd1c42735e94625eb351ffbb218152
Satoki <satokiito@gmail.com>
2025-01-14 16:05:56 +0000

    feat: create fixPlaylistAlbumName function

--- src/app/playlist/components/PlaylistTracks.jsx
+++ src/app/playlist/components/PlaylistTracks.jsx
@@ -8,7 +8,7 @@ import MenuDots from "../../../assets/svgs/main-app/menu-dots.svg";
 import PlayButton from "../../../assets/svgs/main-app/play-triangle.svg";
 import GetSearchTrackArtists from "../../../shared-functions/GetSearchTrackArtists";
 
-// import fixPlaylistAlbumName from "../../../shared-functions/fixPlaylistAlbumName";
+import fixPlaylistAlbumName from "@/shared-functions/fixPlaylistAlbumName";
 // import fixPlaylistTrackName from "../../../shared-functions/fixPlaylistTrackName";
 // import formatDateShortMths from "../../../shared-functions/formatDateShortMths";
 
