ffdfbcea54433bc2e4d16bb8c9ad20902bf72ef2
Satoki <satokiito@gmail.com>
2025-01-14 16:12:49 +0000

    refactor: remove dead code from layout file

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,42 +1,4 @@
-// import { Baloo_Chettan_2, Calligraffitti, Figtree } from "next/font/google";
-// import "./globals.css";
-
-// const balooChettan2 = Baloo_Chettan_2({
-//   variable: "--font-baloo-chettan-2",
-//   subsets: ["latin"],
-// });
-
-// const calligraffitti = Calligraffitti({
-//   variable: "--font-calligraffitti",
-//   weight: "400",
-//   subsets: ["latin"],
-// });
-
-// const figtree = Figtree({
-//   variable: "--font-figtree",
-//   subsets: ["latin"],
-// });
-
-// export const metadata = {
-//   title: "Spotify2",
-//   description: "A Spotify clone app",
-// };
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en">
-//       <body
-//         className={`${balooChettan2.variable} ${calligraffitti.variable} ${figtree.variable}`}
-//       >
-//         {children}
-//       </body>
-//     </html>
-//   );
-// }
-
-// app/layout.jsx
 import { Baloo_Chettan_2, Calligraffitti, Figtree } from "next/font/google";
-// import ClientWrapper from "@/components/ClientWrapper";
 import ClientWrapper from "./ClientWrapper";
 import "./globals.css";
 import "./root.css";
