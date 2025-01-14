2343f14d537b8236ce7c472d866f713b07efc115
Satoki <satokiito@gmail.com>
2025-01-14 15:35:54 +0000

    feat: add content to Playlist page

--- src/app/playlist/page.jsx
+++ src/app/playlist/page.jsx
@@ -1,7 +1,50 @@
-function Playlist() {
-    return (
-        <div></div>
-    )
+import { useEffect, useState } from "react";
+import { usePalette } from "react-palette";
+import { useRouter } from "next/navigation";
+
+// import getPlaylist from "./functions/getPlaylist";
+// import sleep from "../../shared-functions/sleep";
+// import palletGradientPlaylist from "../../ColorThief/paletteGradientPlaylist";
+// import PlaylistTopNav from "./components/PlaylistTopNav"
+// import PlaylistBanner from "./components/PlaylistBanner";
+// import PlaylistControls from "./components/PlaylistControls";
+// import PlaylistTracks from "./components/PlaylistTracks";
+
+import "./styles/playlist-page.css"
+
+function Playlist({ token, setToken }) {
+  const router = useRouter();
+  const { playlistId } = router.query;
+
+  const [playlistInfo, setPlaylistInfo] = useState({});
+  const [playlistTracks, setPlaylistTracks] = useState([]);
+
+  useEffect(() => {
+    sleep(0).then(() =>
+      getPlaylist(token, playlistId, setPlaylistInfo, setPlaylistTracks)
+    );
+  }, [playlistId, token]);
+
+  const { data } = usePalette(playlistInfo.img);
+  useEffect(() => {
+    sleep(0).then(() => palletGradientPlaylist(data));
+  }, [data]);
+
+  return (
+    <div className="scrollbar-playlist">
+      <section className="playlist-page--container grid">
+        <PlaylistTopNav setToken={setToken} />
+        <PlaylistBanner
+          playlistInfo={playlistInfo}
+          playlistTracks={playlistTracks}
+        />
+        <div className="playlist-page--sub-container grid">
+          <PlaylistControls />
+          <PlaylistTracks playlistTracks={playlistTracks} />
+        </div>
+      </section>
+    </div>
+  );
 }
 
-export default Playlist
+export default Playlist;
