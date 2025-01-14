0f53f3cf8eb4e883ae5079a93cfd4c1b8030dece
Satoki <satokiito@gmail.com>
2025-01-14 16:17:55 +0000

    feat: add content to UserPlaylist component

--- src/app/user-playlist/[playlistId]/page.jsx
+++ src/app/user-playlist/[playlistId]/page.jsx
@@ -1,7 +1,102 @@
-function UserPlaylist() {
-    return (
+"use client";
 
-    )
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useClient } from "@/components/ClientContext";
+import { usePalette } from "react-palette";
+import axios from "axios";
+import sleep from "@/shared-functions/sleep";
+
+// import paletteGradientUserPlaylist from "../../ColorThief/paletteGradientUserPlaylist";
+// import UserPlaylistTopNav from "./components/UserPlaylistTopNav";
+// import UserPlaylistBanner from "./components/UserPlaylistBanner";
+// import UserPlaylistControls from "./components/UserPlaylistControls";
+// import UserPlaylistTracks from "./components/UserPlaylistTracks";
+
+// import "./styles/user-playlist.css";
+
+function UserPlaylist() {
+  const { token, setToken } = useClient();
+
+  const router = useRouter();
+  const { playlistId } = router.query;
+
+  const [userPlaylistInfo, setUserPlaylistInfo] = useState({});
+  const [userPlaylistTracks, setUserPlaylistTracks] = useState([]);
+
+  useEffect(() => {
+    const getUserPlaylistInfo = async () => {
+      const { data } = await axios.get(
+        `https://api.spotify.com/v1/playlists/${playlistId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          params: {
+            limit: 20,
+          },
+        }
+      );
+      console.log(data);
+      setUserPlaylistInfo({
+        owner: data.owner.display_name,
+        name: data.name,
+        img: data.images[0].url,
+        description: data.description,
+        numSongs: data.tracks.total,
+        isPublic: data.public,
+      });
+    };
+    getUserPlaylistInfo();
+  }, [playlistId, token]);
+
+  useEffect(() => {
+    let playlistURL = `https://api.spotify.com/v1/playlists/${playlistId}/tracks`;
+    let userPlaylistSongs = [];
+    let combinedPlaylistSongs = [];
+    const getUserPlaylistTracks = async () => {
+      const { data } = await axios.get(playlistURL, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        params: {
+          limit: 50,
+        },
+      });
+      userPlaylistSongs.push(data.items);
+      if (data.next !== null) {
+        playlistURL = data.next;
+        getUserPlaylistTracks();
+      } else {
+        userPlaylistSongs.forEach((array) => {
+          combinedPlaylistSongs = combinedPlaylistSongs.concat(array);
+        });
+      }
+      setUserPlaylistTracks(combinedPlaylistSongs);
+    };
+    getUserPlaylistTracks();
+  }, [playlistId, token]);
+
+  const { data } = usePalette(userPlaylistInfo.img);
+  useEffect(() => {
+    sleep(0).then(() => paletteGradientUserPlaylist(data));
+  }, [data]);
+
+  return (
+    <div className="scrollbar-user-playlist">
+      <section className="user-playlist--container grid">
+        <UserPlaylistTopNav setToken={setToken} />
+        <UserPlaylistBanner
+          userPlaylistInfo={userPlaylistInfo}
+          userPlaylistTracks={userPlaylistTracks}
+        />
+        <div className="user-playlist--sub-container grid">
+          <UserPlaylistControls />
+          <UserPlaylistTracks userPlaylistTracks={userPlaylistTracks} />
+        </div>
+      </section>
+    </div>
+  );
 }
 
-export default UserPlaylist
+export default UserPlaylist;
