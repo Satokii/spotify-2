098adb96a1bfb9f084a4b4605e4d31e2d13d95f5
Satoki <satokiito@gmail.com>
2025-01-15 17:48:50 +0000

    feat: create ArtistTopNav component

---
+++ src/app/artist/components/ArtistTopNav.jsx
@@ -0,0 +1,14 @@
+import MainNav from "@/components/main-nav";
+
+// import "../styles/artist-top-nav.css";
+
+function ArtistTopNav({ setToken }) {
+  return (
+    <div className="artist-page--menu-backing">
+      <section className="artist-page--menu-container grid">
+        <MainNav setToken={setToken} />
+      </section>
+    </div>
+  );
+}
+export default ArtistTopNav;
