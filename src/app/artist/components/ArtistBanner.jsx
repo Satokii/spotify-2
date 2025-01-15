1cb78a7c49d94ac8d0a5c120335ca275d046d3f5
Satoki <satokiito@gmail.com>
2025-01-15 17:51:29 +0000

    feat: create ArtistBanner component

---
+++ src/app/artist/components/ArtistBanner.jsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+
+// import VerifiedIcon from "../../../assets/svgs/main-app/verified-icon.svg";
+
+// import "../styles/artist-banner.css";
+
+function ArtistBanner({ artistInfo }) {
+  return (
+    <div
+      className="artist-page--banner-container grid"
+      style={{
+        backgroundImage: `url(${artistInfo.img})`,
+        backgroundPosition: "50% 45%",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "75%",
+      }}
+    >
+      <div className="artist-page--banner-filter grid">
+        <div className="artist-banner--verified-container grid">
+          <Image
+            className="artist-banner--verified-img"
+            src={VerifiedIcon}
+            alt="verified icon"
+          />
+          <p className="artist-banner--verified-text">Verified Artist</p>
+        </div>
+        <div className="artist-banner--artist-name grid ">
+          {artistInfo.name}
+        </div>
+        <div className="artist-banner--artist-followers">{`${artistInfo.followers} followers`}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ArtistBanner;
