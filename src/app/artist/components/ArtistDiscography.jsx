dbc3cd5f59bf942a8044197bd322df17d1a99333
Satoki <satokiito@gmail.com>
2025-01-15 18:05:43 +0000

    feat: import svg for ArtistDiscography component

--- src/app/artist/components/ArtistDiscography.jsx
+++ src/app/artist/components/ArtistDiscography.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import getYear from "@/shared-functions/getYear";
 import scrollToTop from "@/shared-functions/scrollToTop";
 
-// import GrayCircle from "../../../assets/svgs/main-app/gray-circle.svg";
+import GrayCircle from "../../../assets/svgs/main-app/gray-circle.svg";
 import PlayGreen from "../../../assets/svgs/main-app/main-play-btn.svg";
 
 // import "../styles/artist-discography.css";
