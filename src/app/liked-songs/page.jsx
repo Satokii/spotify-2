8d48aeb7d8ba3b6ca854c833b733ebef641b94eb
Satoki <satokiito@gmail.com>
2025-01-15 16:02:28 +0000

    feat: add content to LikedSongs page

--- src/app/liked-songs/page.jsx
+++ src/app/liked-songs/page.jsx
@@ -1,7 +1,55 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { usePalette } from "react-palette";
+import { useClient } from "@/components/ClientContext";
+import axios from "axios";
+import sleep from "@/shared-functions/sleep";
+
+// import paletteGradientLikedSongs from "../../ColorThief/paletteGradientLikedSongs";
+import LikedSongsImg from "../../assets/images/liked-songs-img.png";
+// import LikedSongsTopNav from "./components/LikedSongsTopNav"
+// import LikedSongsBanner from "./components/LikedSongsBanner";
+// import LikedSongsControls from "./components/LikedSongsControls";
+// import LikedSongsTracks from "./components/LikedSongsTracks";
+
+// import "./styles/liked-songs.css"
+
 function LikedSongs() {
-    return (
-        <div></div>
-    )
+  const { token, setToken } = useClient();
+  const [likedSongsInfo, setLikedSongsInfo] = useState([]);
+  const [likedSongs, setLikedSongs] = useState([]);
+
+  useEffect(() => {
+    const getLikedSongs = async () => {
+      const { data } = await axios.get(`https://api.spotify.com/v1/me/tracks`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setLikedSongsInfo({ trackNum: data.total });
+      setLikedSongs(data.items);
+    };
+    getLikedSongs();
+  }, [token]);
+
+  const { data } = usePalette(LikedSongsImg);
+  useEffect(() => {
+    sleep(0).then(() => paletteGradientLikedSongs(data));
+  }, [data]);
+
+  return (
+    <div className="scrollbar-liked-songs">
+      <section className="liked-songs--container grid">
+        <LikedSongsTopNav setToken={setToken} />
+        <LikedSongsBanner likedSongsInfo={likedSongsInfo} />
+        <div className="liked-songs--sub-container grid">
+          <LikedSongsControls />
+          <LikedSongsTracks likedSongs={likedSongs} />
+        </div>
+      </section>
+    </div>
+  );
 }
 
-export default LikedSongs
+export default LikedSongs;
