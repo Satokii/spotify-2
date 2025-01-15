ad1fb9051a1a4e7964fa95964a3f806bc50588dc
Satoki <satokiito@gmail.com>
2025-01-15 16:41:11 +0000

    feat: create LikedSongsTopNav component

---
+++ src/app/liked-songs/components/LikedSongsTopNav.jsx
@@ -0,0 +1,15 @@
+import MainNav from "@/components/main-nav";
+
+// import "../styles/liked-songs-top-nav.css";
+
+function LikedSongsTopNav({ setToken }) {
+  return (
+    <div className="liked-songs--menu-backing">
+      <section className="liked-songs--menu-container grid">
+        <MainNav setToken={setToken} />
+      </section>
+    </div>
+  );
+}
+
+export default LikedSongsTopNav;
