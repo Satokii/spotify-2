751dba3d963fd0ad0e93486be93e120e1348c361
Satoki <satokiito@gmail.com>
2025-01-16 14:00:10 +0000

    feat: add content to RenderDashboardPlaylists component

--- src/components/dashboard/components/RenderDashboardPlaylists.jsx
+++ src/components/dashboard/components/RenderDashboardPlaylists.jsx
@@ -1,6 +1,22 @@
-function RenderDashboardPlaylists() {
+"use client"
+
+import { useEffect, useState } from "react";
+// import DashboardCardTemplate from "./DashboardCardTemplate";
+// import getDashboardPlaylists from "../functions/getDashboardPlaylists";
+
+function RenderDashboardPlaylists({ token, category, title }) {
+    const [playlist, setPlaylist] = useState([])
+
+    useEffect(() => {
+        const getPopPlaylists = async () => {
+            const data = await getDashboardPlaylists(token, category)
+            setPlaylist(data.playlists.items)
+        };
+        getPopPlaylists()
+    }, [category, token])
+    
     return (
-        <div></div>
+        <DashboardCardTemplate title={title} itemArr={playlist} />
     )
 }
 
