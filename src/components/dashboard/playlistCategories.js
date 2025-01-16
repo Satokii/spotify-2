a44981961c4410999a81feab0ed38ed37fcc5499
Satoki <satokiito@gmail.com>
2025-01-16 14:37:40 +0000

    feat: add more data to playlistCategories file

--- src/components/dashboard/playlistCategories.js
+++ src/components/dashboard/playlistCategories.js
@@ -1,83 +1,470 @@
 export const playlistCategories = [
-    {
-        category: "pop",
-        title: "Pop",
-        details: [
-            {
-                name: "test 1",
-                img: "https//",
-                description: "this is the description"
-            },
-            {
-                name: "test 2",
-                img: "https//",
-                description: "this is the description"
-            },{
-                name: "test 3",
-                img: "https//",
-                description: "this is the description"
-            },{
-                name: "test 4",
-                img: "https//",
-                description: "this is the description"
-            },{
-                name: "test 5",
-                img: "https//",
-                description: "this is the description"
-            },{
-                name: "test 6",
-                img: "https//",
-                description: "this is the description"
-            },
-        ]
-        
-    },
-//     {
-//         category: "rock",
-//         title: "Rock"
-//     },
-//     {
-//         category: "hiphop",
-//         title: "Hip-Hop"
-//     },
-//     {
-//         category: "mood",
-//         title: "Mood"
-//     },
-//     {
-//         category: "party",
-//         title: "Party"
-//     },
-//     {
-//         category: "kpop",
-//         title: "K-Pop"
-//     },
-//     {
-//         category: "indie",
-//         title: "Indie"
-//     },
-//     {
-//         category: "throwback",
-//         title: "Throwback"
-//     },
-//     {
-//         category: "0JQ5DAqbMKFEZPnFQSFB1T",
-//         title: "R&B"
-//     },
-//     {
-//         category: "jazz",
-//         title: "Jazz"
-//     },
-//     {
-//         category: "classical",
-//         title: "Classical"
-//     },
-//     {
-//         category: "anime",
-//         title: "Anime"
-//     },
-//     {
-//         category: "workout",
-//         title: "Workout"
-//     }
-]
+  {
+    category: "pop",
+    title: "Pop",
+    details: [
+      {
+        name: "test 1",
+        img: "https://th.bing.com/th/id/OIP.X6Bv0OhuZCjTWHKbPABwjAHaHa?w=182&h=182&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        description: "this is the description",
+      },
+      {
+        name: "test 2",
+        img: "https://th.bing.com/th/id/OIP.X6Bv0OhuZCjTWHKbPABwjAHaHa?w=182&h=182&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        description: "this is the description",
+      },
+      {
+        name: "test 3",
+        img: "https://th.bing.com/th/id/OIP.X6Bv0OhuZCjTWHKbPABwjAHaHa?w=182&h=182&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        description: "this is the description",
+      },
+      {
+        name: "test 4",
+        img: "https://th.bing.com/th/id/OIP.X6Bv0OhuZCjTWHKbPABwjAHaHa?w=182&h=182&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        description: "this is the description",
+      },
+      {
+        name: "test 5",
+        img: "https://th.bing.com/th/id/OIP.X6Bv0OhuZCjTWHKbPABwjAHaHa?w=182&h=182&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        description: "this is the description",
+      },
+      {
+        name: "test 6",
+        img: "https://th.bing.com/th/id/OIP.X6Bv0OhuZCjTWHKbPABwjAHaHa?w=182&h=182&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        description: "this is the description",
+      },
+    ],
+  },
+  {
+    category: "rock",
+    title: "Rock",
+    details: [
+      {
+        name: "test 1",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 2",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 3",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 4",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 5",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 6",
+        img: "https//",
+        description: "this is the description",
+      },
+    ],
+  },
+  {
+    category: "hiphop",
+    title: "Hip-Hop",
+    details: [
+      {
+        name: "test 1",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 2",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 3",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 4",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 5",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 6",
+        img: "https//",
+        description: "this is the description",
+      },
+    ],
+  },
+  {
+    category: "mood",
+    title: "Mood",
+    details: [
+      {
+        name: "test 1",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 2",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 3",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 4",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 5",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 6",
+        img: "https//",
+        description: "this is the description",
+      },
+    ],
+  },
+  {
+    category: "party",
+    title: "Party",
+    details: [
+      {
+        name: "test 1",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 2",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 3",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 4",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 5",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 6",
+        img: "https//",
+        description: "this is the description",
+      },
+    ],
+  },
+  {
+    category: "kpop",
+    title: "K-Pop",
+    details: [
+      {
+        name: "test 1",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 2",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 3",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 4",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 5",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 6",
+        img: "https//",
+        description: "this is the description",
+      },
+    ],
+  },
+  {
+    category: "indie",
+    title: "Indie",
+    details: [
+      {
+        name: "test 1",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 2",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 3",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 4",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 5",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 6",
+        img: "https//",
+        description: "this is the description",
+      },
+    ],
+  },
+  {
+    category: "throwback",
+    title: "Throwback",
+    details: [
+      {
+        name: "test 1",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 2",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 3",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 4",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 5",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 6",
+        img: "https//",
+        description: "this is the description",
+      },
+    ],
+  },
+  {
+    category: "R&B",
+    title: "R&B",
+    details: [
+      {
+        name: "test 1",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 2",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 3",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 4",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 5",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 6",
+        img: "https//",
+        description: "this is the description",
+      },
+    ],
+  },
+  {
+    category: "jazz",
+    title: "Jazz",
+    details: [
+      {
+        name: "test 1",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 2",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 3",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 4",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 5",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 6",
+        img: "https//",
+        description: "this is the description",
+      },
+    ],
+  },
+  {
+    category: "classical",
+    title: "Classical",
+    details: [
+      {
+        name: "test 1",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 2",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 3",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 4",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 5",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 6",
+        img: "https//",
+        description: "this is the description",
+      },
+    ],
+  },
+  {
+    category: "anime",
+    title: "Anime",
+    details: [
+      {
+        name: "test 1",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 2",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 3",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 4",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 5",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 6",
+        img: "https//",
+        description: "this is the description",
+      },
+    ],
+  },
+  {
+    category: "workout",
+    title: "Workout",
+    details: [
+      {
+        name: "test 1",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 2",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 3",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 4",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 5",
+        img: "https//",
+        description: "this is the description",
+      },
+      {
+        name: "test 6",
+        img: "https//",
+        description: "this is the description",
+      },
+    ],
+  },
+];
