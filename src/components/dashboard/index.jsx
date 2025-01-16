b39845226a8420c05c2ca508ea839520461c98f2
Satoki <satokiito@gmail.com>
2025-01-16 13:38:39 +0000

    feat: add sub-section content to dashboard

--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -1,19 +1,43 @@
-import DashboardMenu from "./components/DashboardMenu"
-import DashboardBanner from "./components/DashboardBanner"
+import DashboardMenu from "./components/DashboardMenu";
+import DashboardBanner from "./components/DashboardBanner";
 
-import "./styles/dashboard.css"
+import "./styles/dashboard.css";
 
-function Dashboard({ token, queue, setQueue, setToken, currentTrack, notPlaying }) {
-    return (
-        <div className="dashboard-outer-container grid">
-            <div className="scrollbar-dashboard">
-                <main className="dashboard grid">
-                 <DashboardMenu setToken={setToken} />
-                 <DashboardBanner token={token} queue={queue} setQueue={setQueue} currentTrack={currentTrack} notPlaying={notPlaying} />
-                </main>
-            </div>
-        </div>
-    )
+function Dashboard({
+  token,
+  queue,
+  setQueue,
+  setToken,
+  currentTrack,
+  notPlaying,
+}) {
+  return (
+    <div className="dashboard-outer-container grid">
+      <div className="scrollbar-dashboard">
+        <main className="dashboard grid">
+          <DashboardMenu setToken={setToken} />
+          <DashboardBanner
+            token={token}
+            queue={queue}
+            setQueue={setQueue}
+            currentTrack={currentTrack}
+            notPlaying={notPlaying}
+          />
+          <section className="dashboard--sub-container grid">
+            <DashboardTopFeaturedPlaylists token={token} />
+            {playlistCategories.map((cat, index) => (
+              <RenderDashboardPlaylists
+                key={`${cat.category}-${index}`}
+                token={token}
+                category={cat.category}
+                title={cat.title}
+              />
+            ))}
+          </section>
+        </main>
+      </div>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
