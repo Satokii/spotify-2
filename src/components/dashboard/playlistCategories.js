da04387efb8f7f4ee4c92c9ba6867a1440cf7deb
Satoki <satokiito@gmail.com>
2025-01-17 15:24:16 +0000

    feat: add id to each playlist track in data

--- src/components/dashboard/playlistCategories.js
+++ src/components/dashboard/playlistCategories.js
@@ -5,31 +5,37 @@ export const playlistCategories = [
     details: [
       {
         name: "Playlist 1",
+        id: 1,
         img: "https://th.bing.com/th/id/OIP.X6Bv0OhuZCjTWHKbPABwjAHaHa?w=182&h=182&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 2",
+        id: 2,
         img: "https://th.bing.com/th/id/OIP.X6Bv0OhuZCjTWHKbPABwjAHaHa?w=182&h=182&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 3",
+        id: 3,
         img: "https://th.bing.com/th/id/OIP.X6Bv0OhuZCjTWHKbPABwjAHaHa?w=182&h=182&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 4",
+        id: 4,
         img: "https://th.bing.com/th/id/OIP.X6Bv0OhuZCjTWHKbPABwjAHaHa?w=182&h=182&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 5",
+        id: 5,
         img: "https://th.bing.com/th/id/OIP.X6Bv0OhuZCjTWHKbPABwjAHaHa?w=182&h=182&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 6",
+        id: 6,
         img: "https://th.bing.com/th/id/OIP.X6Bv0OhuZCjTWHKbPABwjAHaHa?w=182&h=182&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
@@ -41,31 +47,37 @@ export const playlistCategories = [
     details: [
       {
         name: "Playlist 1",
+        id: 7,
         img: "https://th.bing.com/th/id/OIP.FduqH2omwjTUl38Q6eqICAHaHa?w=192&h=191&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 2",
+        id: 8,
         img: "https://th.bing.com/th/id/OIP.FduqH2omwjTUl38Q6eqICAHaHa?w=192&h=191&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 3",
+        id: 9,
         img: "https://th.bing.com/th/id/OIP.FduqH2omwjTUl38Q6eqICAHaHa?w=192&h=191&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 4",
+        id: 10,
         img: "https://th.bing.com/th/id/OIP.FduqH2omwjTUl38Q6eqICAHaHa?w=192&h=191&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 5",
+        id: 11,
         img: "https://th.bing.com/th/id/OIP.FduqH2omwjTUl38Q6eqICAHaHa?w=192&h=191&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 6",
+        id: 12,
         img: "https://th.bing.com/th/id/OIP.FduqH2omwjTUl38Q6eqICAHaHa?w=192&h=191&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
@@ -77,31 +89,37 @@ export const playlistCategories = [
     details: [
       {
         name: "Playlist 1",
+        id: 13,
         img: "https://th.bing.com/th/id/OIP.Nk61v0P3BKzP3P936XBRFAHaF2?w=237&h=188&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 2",
+        id: 14,
         img: "https://th.bing.com/th/id/OIP.Nk61v0P3BKzP3P936XBRFAHaF2?w=237&h=188&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 3",
+        id: 15,
         img: "https://th.bing.com/th/id/OIP.Nk61v0P3BKzP3P936XBRFAHaF2?w=237&h=188&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 4",
+        id: 16,
         img: "https://th.bing.com/th/id/OIP.Nk61v0P3BKzP3P936XBRFAHaF2?w=237&h=188&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 5",
+        id: 17,
         img: "https://th.bing.com/th/id/OIP.Nk61v0P3BKzP3P936XBRFAHaF2?w=237&h=188&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 6",
+        id: 18,
         img: "https://th.bing.com/th/id/OIP.Nk61v0P3BKzP3P936XBRFAHaF2?w=237&h=188&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
@@ -113,31 +131,37 @@ export const playlistCategories = [
     details: [
       {
         name: "Playlist 1",
+        id: 19,
         img: "https://th.bing.com/th/id/OIP.-XRNL7zQVO9-DVhs3V0f7QHaHa?w=161&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 2",
+        id: 20,
         img: "https://th.bing.com/th/id/OIP.-XRNL7zQVO9-DVhs3V0f7QHaHa?w=161&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 3",
+        id: 21,
         img: "https://th.bing.com/th/id/OIP.-XRNL7zQVO9-DVhs3V0f7QHaHa?w=161&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 4",
+        id: 22,
         img: "https://th.bing.com/th/id/OIP.-XRNL7zQVO9-DVhs3V0f7QHaHa?w=161&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 5",
+        id: 23,
         img: "https://th.bing.com/th/id/OIP.-XRNL7zQVO9-DVhs3V0f7QHaHa?w=161&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 6",
+        id: 24,
         img: "https://th.bing.com/th/id/OIP.-XRNL7zQVO9-DVhs3V0f7QHaHa?w=161&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
@@ -149,31 +173,37 @@ export const playlistCategories = [
     details: [
       {
         name: "Playlist 1",
+        id: 25,
         img: "https://th.bing.com/th?q=Best+DJ+Party+Music&w=120&h=120&c=1&rs=1&qlt=90&cb=1&dpr=1.3&pid=InlineBlock&mkt=ja-JP&cc=JP&setlang=en&adlt=moderate&t=1&mw=247",
         description: "this is the description",
       },
       {
         name: "Playlist 2",
+        id: 26,
         img: "https://th.bing.com/th?q=Best+DJ+Party+Music&w=120&h=120&c=1&rs=1&qlt=90&cb=1&dpr=1.3&pid=InlineBlock&mkt=ja-JP&cc=JP&setlang=en&adlt=moderate&t=1&mw=247",
         description: "this is the description",
       },
       {
         name: "Playlist 3",
+        id: 27,
         img: "https://th.bing.com/th?q=Best+DJ+Party+Music&w=120&h=120&c=1&rs=1&qlt=90&cb=1&dpr=1.3&pid=InlineBlock&mkt=ja-JP&cc=JP&setlang=en&adlt=moderate&t=1&mw=247",
         description: "this is the description",
       },
       {
         name: "Playlist 4",
+        id: 28,
         img: "https://th.bing.com/th?q=Best+DJ+Party+Music&w=120&h=120&c=1&rs=1&qlt=90&cb=1&dpr=1.3&pid=InlineBlock&mkt=ja-JP&cc=JP&setlang=en&adlt=moderate&t=1&mw=247",
         description: "this is the description",
       },
       {
         name: "Playlist 5",
+        id: 29,
         img: "https://th.bing.com/th?q=Best+DJ+Party+Music&w=120&h=120&c=1&rs=1&qlt=90&cb=1&dpr=1.3&pid=InlineBlock&mkt=ja-JP&cc=JP&setlang=en&adlt=moderate&t=1&mw=247",
         description: "this is the description",
       },
       {
         name: "Playlist 6",
+        id: 30,
         img: "https://th.bing.com/th?q=Best+DJ+Party+Music&w=120&h=120&c=1&rs=1&qlt=90&cb=1&dpr=1.3&pid=InlineBlock&mkt=ja-JP&cc=JP&setlang=en&adlt=moderate&t=1&mw=247",
         description: "this is the description",
       },
@@ -185,31 +215,37 @@ export const playlistCategories = [
     details: [
       {
         name: "Playlist 1",
+        id: 31,
         img: "https://th.bing.com/th/id/OIP.YvjJgFrFcGMyVd6S2y2tBgHaHa?w=160&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 2",
+        id: 32,
         img: "https://th.bing.com/th/id/OIP.YvjJgFrFcGMyVd6S2y2tBgHaHa?w=160&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 3",
+        id: 33,
         img: "https://th.bing.com/th/id/OIP.YvjJgFrFcGMyVd6S2y2tBgHaHa?w=160&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 4",
+        id: 34,
         img: "https://th.bing.com/th/id/OIP.YvjJgFrFcGMyVd6S2y2tBgHaHa?w=160&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 5",
+        id: 35,
         img: "https://th.bing.com/th/id/OIP.YvjJgFrFcGMyVd6S2y2tBgHaHa?w=160&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 6",
+        id: 36,
         img: "https://th.bing.com/th/id/OIP.YvjJgFrFcGMyVd6S2y2tBgHaHa?w=160&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
@@ -221,31 +257,37 @@ export const playlistCategories = [
     details: [
       {
         name: "Playlist 1",
+        id: 37,
         img: "https://th.bing.com/th/id/OIP.4lfTxOQlytdkKhPZrCvDqgHaHa?w=200&h=200&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 2",
+        id: 38,
         img: "https://th.bing.com/th/id/OIP.4lfTxOQlytdkKhPZrCvDqgHaHa?w=200&h=200&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 3",
+        id: 39,
         img: "https://th.bing.com/th/id/OIP.4lfTxOQlytdkKhPZrCvDqgHaHa?w=200&h=200&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 4",
+        id: 40,
         img: "https://th.bing.com/th/id/OIP.4lfTxOQlytdkKhPZrCvDqgHaHa?w=200&h=200&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 5",
+        id: 41,
         img: "https://th.bing.com/th/id/OIP.4lfTxOQlytdkKhPZrCvDqgHaHa?w=200&h=200&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 6",
+        id: 42,
         img: "https://th.bing.com/th/id/OIP.4lfTxOQlytdkKhPZrCvDqgHaHa?w=200&h=200&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
@@ -257,31 +299,37 @@ export const playlistCategories = [
     details: [
       {
         name: "Playlist 1",
+        id: 43,
         img: "https://th.bing.com/th/id/OIP.YfH0AlZUFDgsK3T0BtcBeAHaHa?w=159&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 2",
+        id: 44,
         img: "https://th.bing.com/th/id/OIP.YfH0AlZUFDgsK3T0BtcBeAHaHa?w=159&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 3",
+        id: 45,
         img: "https://th.bing.com/th/id/OIP.YfH0AlZUFDgsK3T0BtcBeAHaHa?w=159&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 4",
+        id: 46,
         img: "https://th.bing.com/th/id/OIP.YfH0AlZUFDgsK3T0BtcBeAHaHa?w=159&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 5",
+        id: 47,
         img: "https://th.bing.com/th/id/OIP.YfH0AlZUFDgsK3T0BtcBeAHaHa?w=159&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 6",
+        id:48,
         img: "https://th.bing.com/th/id/OIP.YfH0AlZUFDgsK3T0BtcBeAHaHa?w=159&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
@@ -293,31 +341,37 @@ export const playlistCategories = [
     details: [
       {
         name: "Playlist 1",
+        id: 49,
         img: "https://th.bing.com/th/id/OIP.MOog_JS9qqEM7nNUz-xZpwHaHV?w=200&h=198&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 2",
+        id: 50,
         img: "https://th.bing.com/th/id/OIP.MOog_JS9qqEM7nNUz-xZpwHaHV?w=200&h=198&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 3",
+        id: 51,
         img: "https://th.bing.com/th/id/OIP.MOog_JS9qqEM7nNUz-xZpwHaHV?w=200&h=198&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 4",
+        id: 52,
         img: "https://th.bing.com/th/id/OIP.MOog_JS9qqEM7nNUz-xZpwHaHV?w=200&h=198&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 5",
+        id: 53,
         img: "https://th.bing.com/th/id/OIP.MOog_JS9qqEM7nNUz-xZpwHaHV?w=200&h=198&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 6",
+        id: 54,
         img: "https://th.bing.com/th/id/OIP.MOog_JS9qqEM7nNUz-xZpwHaHV?w=200&h=198&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
@@ -329,31 +383,37 @@ export const playlistCategories = [
     details: [
       {
         name: "Playlist 1",
+        id: 55,
         img: "http://ts1.mm.bing.net/th?id=OIP.4h_9cRQqfXrfiQXOtgB8vwHaHa&pid=15.1",
         description: "this is the description",
       },
       {
         name: "Playlist 2",
+        id: 56,
         img: "http://ts1.mm.bing.net/th?id=OIP.4h_9cRQqfXrfiQXOtgB8vwHaHa&pid=15.1",
         description: "this is the description",
       },
       {
         name: "Playlist 3",
+        id: 57,
         img: "http://ts1.mm.bing.net/th?id=OIP.4h_9cRQqfXrfiQXOtgB8vwHaHa&pid=15.1",
         description: "this is the description",
       },
       {
         name: "Playlist 4",
+        id: 58,
         img: "http://ts1.mm.bing.net/th?id=OIP.4h_9cRQqfXrfiQXOtgB8vwHaHa&pid=15.1",
         description: "this is the description",
       },
       {
         name: "Playlist 5",
+        id: 59,
         img: "http://ts1.mm.bing.net/th?id=OIP.4h_9cRQqfXrfiQXOtgB8vwHaHa&pid=15.1",
         description: "this is the description",
       },
       {
         name: "Playlist 6",
+        id: 60,
         img: "http://ts1.mm.bing.net/th?id=OIP.4h_9cRQqfXrfiQXOtgB8vwHaHa&pid=15.1",
         description: "this is the description",
       },
@@ -365,31 +425,37 @@ export const playlistCategories = [
     details: [
       {
         name: "Playlist 1",
+        id: 61,
         img: "http://ts2.mm.bing.net/th?id=OIP.HBfkP0r-qE9RtYWvNS5AswHaHa&pid=15.1",
         description: "this is the description",
       },
       {
         name: "Playlist 2",
+        id: 62,
         img: "http://ts2.mm.bing.net/th?id=OIP.HBfkP0r-qE9RtYWvNS5AswHaHa&pid=15.1",
         description: "this is the description",
       },
       {
         name: "Playlist 3",
+        id: 63,
         img: "http://ts2.mm.bing.net/th?id=OIP.HBfkP0r-qE9RtYWvNS5AswHaHa&pid=15.1",
         description: "this is the description",
       },
       {
         name: "Playlist 4",
+        id: 64,
         img: "http://ts2.mm.bing.net/th?id=OIP.HBfkP0r-qE9RtYWvNS5AswHaHa&pid=15.1",
         description: "this is the description",
       },
       {
         name: "Playlist 5",
+        id: 65,
         img: "http://ts2.mm.bing.net/th?id=OIP.HBfkP0r-qE9RtYWvNS5AswHaHa&pid=15.1",
         description: "this is the description",
       },
       {
         name: "Playlist 6",
+        id: 66,
         img: "http://ts2.mm.bing.net/th?id=OIP.HBfkP0r-qE9RtYWvNS5AswHaHa&pid=15.1",
         description: "this is the description",
       },
@@ -401,31 +467,37 @@ export const playlistCategories = [
     details: [
       {
         name: "Playlist 1",
+        id: 67,
         img: "https://th.bing.com/th/id/OIP.bLO3d1tTX-_K_wANPMcfxAHaEo?w=253&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 2",
+        id: 68,
         img: "https://th.bing.com/th/id/OIP.bLO3d1tTX-_K_wANPMcfxAHaEo?w=253&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 3",
+        id: 69,
         img: "https://th.bing.com/th/id/OIP.bLO3d1tTX-_K_wANPMcfxAHaEo?w=253&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 4",
+        id: 70,
         img: "https://th.bing.com/th/id/OIP.bLO3d1tTX-_K_wANPMcfxAHaEo?w=253&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 5",
+        id: 71,
         img: "https://th.bing.com/th/id/OIP.bLO3d1tTX-_K_wANPMcfxAHaEo?w=253&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
       {
         name: "Playlist 6",
+        id: 72,
         img: "https://th.bing.com/th/id/OIP.bLO3d1tTX-_K_wANPMcfxAHaEo?w=253&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
         description: "this is the description",
       },
@@ -437,31 +509,37 @@ export const playlistCategories = [
     details: [
       {
         name: "Playlist 1",
+        id: 73,
         img: "https://th.bing.com/th/id/OIP.NEGIEdTFP8nCuHbyaEHwWgHaHa?w=183&h=183&c=7&r=0&o=5&dpr=1.3&pid=1.7g",
         description: "this is the description",
       },
       {
         name: "Playlist 2",
+        id: 74,
         img: "https://th.bing.com/th/id/OIP.NEGIEdTFP8nCuHbyaEHwWgHaHa?w=183&h=183&c=7&r=0&o=5&dpr=1.3&pid=1.7g",
         description: "this is the description",
       },
       {
         name: "Playlist 3",
+        id: 75,
         img: "https://th.bing.com/th/id/OIP.NEGIEdTFP8nCuHbyaEHwWgHaHa?w=183&h=183&c=7&r=0&o=5&dpr=1.3&pid=1.7g",
         description: "this is the description",
       },
       {
         name: "Playlist 4",
+        id: 76,
         img: "https://th.bing.com/th/id/OIP.NEGIEdTFP8nCuHbyaEHwWgHaHa?w=183&h=183&c=7&r=0&o=5&dpr=1.3&pid=1.7g",
         description: "this is the description",
       },
       {
         name: "Playlist 5",
+        id: 77,
         img: "https://th.bing.com/th/id/OIP.NEGIEdTFP8nCuHbyaEHwWgHaHa?w=183&h=183&c=7&r=0&o=5&dpr=1.3&pid=1.7g",
         description: "this is the description",
       },
       {
         name: "Playlist 6",
+        id: 78,
         img: "https://th.bing.com/th/id/OIP.NEGIEdTFP8nCuHbyaEHwWgHaHa?w=183&h=183&c=7&r=0&o=5&dpr=1.3&pid=1.7g",
         description: "this is the description",
       },
--- src/components/dashboard/playlistFeatured.js
+++ src/components/dashboard/playlistFeatured.js
@@ -1,31 +1,37 @@
 export const playlistFeatured = [
         {
           name: "Featured 1",
+          id: 79,
           img: "https://i.pinimg.com/736x/bc/41/86/bc41860d978d73e30a5bd5be057bb740.jpg",
           description: "This is the description",
         },
         {
           name: "Featured 2",
+          id: 80,
           img: "https://i.pinimg.com/736x/bc/41/86/bc41860d978d73e30a5bd5be057bb740.jpg",
           description: "This is the description",
         },
         {
           name: "Featured 3",
+          id: 81,
           img: "https://i.pinimg.com/736x/bc/41/86/bc41860d978d73e30a5bd5be057bb740.jpg",
           description: "This is the description",
         },
         {
           name: "Featured 4",
+          id: 82,
           img: "https://i.pinimg.com/736x/bc/41/86/bc41860d978d73e30a5bd5be057bb740.jpg",
           description: "This is the description",
         },
         {
           name: "Featured 5",
+          id: 83,
           img: "https://i.pinimg.com/736x/bc/41/86/bc41860d978d73e30a5bd5be057bb740.jpg",
           description: "This is the description",
         },
         {
           name: "Featured 6",
+          id: 84,
           img: "https://i.pinimg.com/736x/bc/41/86/bc41860d978d73e30a5bd5be057bb740.jpg",
           description: "This is the description",
         },
