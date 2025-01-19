d7bd1ff419685d5905d58b250598471e7831b082
Satoki <satokiito@gmail.com>
2025-01-19 15:48:48 +0000

    build: update next config file to include webkit configuration override

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+import { resolve } from "path";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      process: resolve("node_modules/process/browser.js"),
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
