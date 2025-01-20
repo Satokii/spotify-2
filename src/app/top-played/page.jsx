bc9077532c03f2d1351d5cc7739394a11e99c2a6
Satoki <satokiito@gmail.com>
2025-01-20 13:19:11 +0000

    feat: create toggleTopArtistsDate function

--- src/app/top-played/page.jsx
+++ src/app/top-played/page.jsx
@@ -9,7 +9,7 @@ import TopTracksPreview from "./components/TopTracksPreview";
 import TopArtistsPreview from "./components/TopArtistsPreview";
 import RecentlyPlayed from "./components/RecentlyPlayed";
 import toggleTopTracksDate from "@/shared-functions/toggleTopTracksDate";
-// import toggleTopArtistsDate from "../../shared-functions/toggleTopArtistsDate";
+import toggleTopArtistsDate from "@/shared-functions/toggleTopArtistsDate";
 
 // import "./styles/main.css";
 // import "./styles/main-date-filter.css";
