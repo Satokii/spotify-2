5391ae1b8b83e351342ae2956105deb699fb6d8c
Satoki <satokiito@gmail.com>
2025-01-20 15:07:51 +0000

    feat: pass props to top page top nav component

--- src/app/top-played/components/TopPlayedTopNav.jsx
+++ src/app/top-played/components/TopPlayedTopNav.jsx
@@ -2,7 +2,7 @@ import MainNav from "@/components/main-nav";
 
 import "../styles/top-played-top-nav.css"
 
-function TopPlayedTopNav() {
+function TopPlayedTopNav({ setToken }) {
   return (
     <div className="top-played-page--menu-backing">
       <section className="top-played-page--menu-container grid">
