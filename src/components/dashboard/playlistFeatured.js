14cf81345481d3e5d36d4793f4c2d8af67d93251
Satoki <satokiito@gmail.com>
2025-01-21 15:28:30 +0000

    refactor: remove excess featured playlists

--- src/components/dashboard/playlistFeatured.js
+++ src/components/dashboard/playlistFeatured.js
@@ -1,39 +1,26 @@
 export const playlistFeatured = [
-        {
-          name: "Template Playlists",
-          id: 79,
-          img: "https://th.bing.com/th/id/OIP.0eYKlVvSfLF3fFWR0749ZQAAAA?rs=1&pid=ImgDetMain",
-          description: "When Spotify deprecates the API :)",
-        },
-        {
-          name: "Quackers",
-          id: 80,
-          img: "https://i.scdn.co/image/ab67706c0000da844a5fd632536b51dc6773db2b",
-          description: "Quack Quack.",
-        },
-        {
-          name: "Moments",
-          id: 81,
-          img: "https://i.pinimg.com/736x/cb/e5/e6/cbe5e65cb89fdded9f7d3663dcdeeaed.jpg",
-          description: "For all those times.",
-        },
-        {
-          name: "The 1975",
-          id: 82,
-          img: "https://thisis-images.spotifycdn.com/37i9dQZF1DZ06evO1X6Ic8-default.jpg",
-          description: "This is The 1975.",
-        },
-        {
-          name: "Featured 5",
-          id: 83,
-          img: "https://i.pinimg.com/736x/bc/41/86/bc41860d978d73e30a5bd5be057bb740.jpg",
-          description: "This is the description",
-        },
-        {
-          name: "Featured 6",
-          id: 84,
-          img: "https://i.pinimg.com/736x/bc/41/86/bc41860d978d73e30a5bd5be057bb740.jpg",
-          description: "This is the description",
-        },
-  ];
-  
+  {
+    name: "Template Playlists",
+    id: 79,
+    img: "https://th.bing.com/th/id/OIP.0eYKlVvSfLF3fFWR0749ZQAAAA?rs=1&pid=ImgDetMain",
+    description: "When Spotify deprecates the API :)",
+  },
+  {
+    name: "Quackers",
+    id: 80,
+    img: "https://i.scdn.co/image/ab67706c0000da844a5fd632536b51dc6773db2b",
+    description: "Quack Quack.",
+  },
+  {
+    name: "Moments",
+    id: 81,
+    img: "https://i.pinimg.com/736x/cb/e5/e6/cbe5e65cb89fdded9f7d3663dcdeeaed.jpg",
+    description: "For all those times.",
+  },
+  {
+    name: "The 1975",
+    id: 82,
+    img: "https://thisis-images.spotifycdn.com/37i9dQZF1DZ06evO1X6Ic8-default.jpg",
+    description: "This is The 1975.",
+  },
+];
