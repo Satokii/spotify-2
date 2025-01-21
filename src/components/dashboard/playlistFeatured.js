82fb0a5db689a7bf9e153564a499c3818d309c45
Satoki <satokiito@gmail.com>
2025-01-21 14:59:56 +0000

    feat: add new featured playlists cover art

--- src/components/dashboard/playlistFeatured.js
+++ src/components/dashboard/playlistFeatured.js
@@ -2,25 +2,25 @@ export const playlistFeatured = [
         {
           name: "Featured 1",
           id: 79,
-          img: "https://i.pinimg.com/736x/bc/41/86/bc41860d978d73e30a5bd5be057bb740.jpg",
+          img: "https://th.bing.com/th/id/OIP.0eYKlVvSfLF3fFWR0749ZQAAAA?rs=1&pid=ImgDetMain",
           description: "This is the description",
         },
         {
           name: "Featured 2",
           id: 80,
-          img: "https://i.pinimg.com/736x/bc/41/86/bc41860d978d73e30a5bd5be057bb740.jpg",
+          img: "https://i.scdn.co/image/ab67706c0000da844a5fd632536b51dc6773db2b",
           description: "This is the description",
         },
         {
           name: "Featured 3",
           id: 81,
-          img: "https://i.pinimg.com/736x/bc/41/86/bc41860d978d73e30a5bd5be057bb740.jpg",
+          img: "https://i.pinimg.com/736x/cb/e5/e6/cbe5e65cb89fdded9f7d3663dcdeeaed.jpg",
           description: "This is the description",
         },
         {
           name: "Featured 4",
           id: 82,
-          img: "https://i.pinimg.com/736x/bc/41/86/bc41860d978d73e30a5bd5be057bb740.jpg",
+          img: "https://thisis-images.spotifycdn.com/37i9dQZF1DZ06evO1X6Ic8-default.jpg",
           description: "This is the description",
         },
         {
