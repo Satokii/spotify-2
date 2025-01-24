16a6d6771b6371d934dcfa5a0cbe1482b2b55669
Satoki <satokiito@gmail.com>
2025-01-24 20:56:35 +0900

    feat: create UserPlaylistBannerEmpty component

---
+++ src/app/user-playlist/components/UserPlaylistBannerEmpty.jsx
@@ -0,0 +1,40 @@
+import convertMsToTime from "../../../shared-functions/convertMsToTime";
+import playlistTimeinMs from "@/app/playlist/functions/playlistTimeInMs";
+
+import "../styles/user-playlist-banner.css";
+
+function UserPlaylistBannerEmpty({ userPlaylistInfo, userPlaylistTracks }) {
+    console.log("check", userPlaylistInfo)
+  return (
+    <div className="user-playlist--banner grid">
+      <div className="user-playlist--banner-img-container">
+          <div className="user-playlist--img-none"></div>
+      </div>
+      <div className="user-playlist--banner-info-container grid">
+        {/* {userPlaylistInfo.isPublic ? (
+          <div className="user-playlist--playlist-type">Public Playlist</div>
+        ) : (
+          <div className="user-playlist--playlist-type">Private Playlist</div>
+        )} */}
+        <div></div>
+        <p className="user-playlist--playlist-name">{userPlaylistInfo.name}</p>
+        <p className="user-playlist--playlist-description">
+          {userPlaylistInfo.description}
+        </p>
+        <div className="user-playlist--playlist-overview-container grid">
+          <p className="user-playlist-overview-owner-name">
+            {userPlaylistInfo.owner}
+          </p>
+          <span className="middot">&middot;</span>
+          <p className="user-playlist-overview-total-tracks">{`${
+            userPlaylistInfo.numSongs
+          } songs, ${convertMsToTime(
+            playlistTimeinMs(userPlaylistTracks)
+          )}`}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserPlaylistBannerEmpty;
